6c6ee1fe6e766fa419dab420ef922b4c7bde29df
rahulravi1026 <rahulravi1026@gmail.com>
2020-07-15 13:02:58 -0700

    First Uploading All Files

---
+++ CustomLoginDemo 3/CustomLoginDemo/Helpers/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Constants.swift
+//  CustomLoginDemo
+//
+//  Created by Rahul Ravi on 2020-07-13.
+//  Copyright © 2020 Rahul Ravi. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    
+    struct Storyboard {
+        
+        static let homeViewController = "HomeVC"
+        
+    }
+    
+    
+}
---
+++ CustomLoginDemo 3/CustomLoginDemo/Helpers/Utilities.swift
@@ -0,0 +1,44 @@
+//
+//  Utilities.swift
+//  customauth
+//
+//  Created by Rahul Ravi on 2020-07-13.
+//  Copyright © 2020 Rahul Ravi. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Utilities {
+    
+    static func styleTextField(_ textfield:UITextField) {
+        
+        // Create the bottom line
+        let bottomLine = CALayer()
+        
+        bottomLine.frame = CGRect(x: 0, y: textfield.frame.height - 2, width: textfield.frame.width - 30, height: 2)
+        bottomLine.backgroundColor = UIColor.init(red: 181/255, green: 126/255, blue: 220/255, alpha: 1).cgColor
+        textfield.borderStyle = .none
+        textfield.layer.addSublayer(bottomLine)
+    }
+    
+    static func styleFilledButton(_ button:UIButton) {
+        button.backgroundColor = UIColor.init(red: 138/255, green: 43/255, blue: 226/255, alpha: 1)
+        button.layer.cornerRadius = 25.0
+        button.tintColor = UIColor.white
+    }
+    
+    static func styleHollowButton(_ button:UIButton) {
+        button.layer.borderWidth = 2
+        button.layer.borderColor = UIColor.black.cgColor
+        button.layer.cornerRadius = 25.0
+        button.backgroundColor = UIColor.white
+        button.tintColor = UIColor.black
+    }
+    
+    static func isPasswordValid(_ password : String) -> Bool {
+        let passwordTest = NSPredicate(format: "SELF MATCHES %@", "^(?=.*[a-z])(?=.*[$@$#!%*?&])[A-Za-z\\d$@$#!%*?&]{8,}")
+        return passwordTest.evaluate(with: password)
+    }
+    
+}
